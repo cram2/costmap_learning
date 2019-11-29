985b81e0f47ee27dd289f2950ca12822d28dea22
Thomas Lipps <lipps.thomas@web.de>
2019-11-29 19:41:08 +0100

    new service: GetSymbolicLocation

--- scripts/service.py
+++ scripts/service.py
@@ -1,15 +1,25 @@
-from costmap_learning.srv import GetCostmap
+#!/usr/bin/env python3
+
+from costmap_learning.srv import GetCostmap, GetSymbolicLocation
 from rospy import init_node, Service, spin
 
-from learning import init_dataset, lookup
+from learning import init_dataset, get_costmap, get_symbolic_location
 
 
-def get_costmap_server():
-    init_node('learning_get_costmap')
-    s = Service('get_costmap', GetCostmap, lookup())
+def start_get_costmap_server():
+    s = Service('get_costmap', GetCostmap, get_costmap)
+
+def start_get_symbolic_location():
+    s = Service('get_symbolic_location', GetSymbolicLocation, get_symbolic_location)
+    
+def start_services():
+    init_node("learning_vr")
+    start_get_costmap_server()
+    start_get_symbolic_location()
+    print("Ready")
     spin()
 
 
 if __name__ == "__main__":
     init_dataset()
-    get_costmap_server()
+    start_services()
